383d0e471b9dba7704e8f964ea1ae76706048c48
Yami Odymel <yamiodymel@gmail.com>
2016-05-07 09:32:49 +0000

    Fixed the pre tag.

--- index.php
+++ index.php
@@ -655,5 +655,21 @@ function minify($buffer)
     return $buffer;
 }
 
-file_put_contents('index.html', minify($content));
+function htmlCompress($html)
+{
+    preg_match_all('!(<(?:code|pre|script).*>[^<]+</(?:code|pre|script)>)!',$html,$pre);
+    $html = preg_replace('!<(?:code|pre).*>[^<]+</(?:code|pre)>!', '#pre#', $html);
+    $html = preg_replace('#<!–[^\[].+–>#', '', $html);
+    $html = preg_replace('/[\r\n\t]+/', ' ', $html);
+    $html = preg_replace('/>[\s]+</', '><', $html);
+    $html = preg_replace('/[\s]+/', ' ', $html);
+    if (!empty($pre[0])) {
+        foreach ($pre[0] as $tag) {
+            $html = preg_replace('!#pre#!', $tag, $html,1);
+        }
+    }
+    return $html;
+}
+
+file_put_contents('index.html', htmlCompress($content));
 ?>
