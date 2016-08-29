fa8e8047439158e3ace4235462941919178f0620
Yami Odymel <yamiodymel@gmail.com>
2016-08-29 21:00:47 +0000

    :wrench: Renamed document components, added paths

--- client/components/DocHeader.vue
+++ client/components/DocHeader.vue
@@ -1,22 +1,89 @@
 <template>
-    <nav class="ts large fluid borderless pure basic menu">
-        <div class="ts container">
-            <a class="item" href="#">Tocas UI</a>
-            <a class="item" href="#">入門教學</a>
-            <a class="item" href="#">一般元素</a>
-            <a class="item" href="#">聚合型</a>
-            <a class="item" href="#">模塊</a>
-            <a class="item" href="#">視圖型</a>
-            <div class="right menu">
-                <a class="item" href="#">Github</a>
-            </div>
-        </div>
-    </nav>
+    <div :class="classes">
+        <h2 class="ts header" :class="classes">
+            {{ title }}
+        </h2>
+        <p v-if="description" v-html="description">
+        </p>
+    </div>
+    
+    
 </template>
 
-<script>
-export default
+<style style="sass" scoped>
+h2
 {
-    name: 'DocHeader'
+    font-size          : 2em !important;
+    border-bottom-width: 2px !important;
+}
+div
+{
+    margin-top   : 3em !important;
+    margin-bottom: 1.5em !important;
+}
+div:first-child
+{
+    margin-top   : 1em !important;
+}
+div.small
+{
+    margin-top: 4em !important;
+}
+.large:not(div)
+{
+    font-size: 2.5em !important;
+}
+.large:not(div) + p
+{
+    font-size  : 1.2em;
+    line-height: 1.8em;
+}
+.small:not(div)
+{
+    font-size: 1.7em !important;
+}
+.tiny:not(div)
+{
+    font-size: 1.3em !important;
+    opacity: 0.9 !important;
+}
+p
+{
+    opacity: 0.7;
+    font-size  : 1.1em;
+    line-height: 1.8em;
+}
+.tiny:not(div) + p
+{
+    opacity: 0.6;
+}
+</style>
+
+<script>
+export default 
+{
+    name : 'DocHeader',
+    props:
+    {
+        title      : {},
+        size       : { default: false },
+        description: { default: false }
+    },
+    computed:
+    {
+        classes()
+        {
+            let classes = [this.size]
+            
+            if(this.size === 'large' || !this.size)
+                classes.push('dividing')
+                
+            return classes
+        },
+        hasSlot()
+        {
+            return this.$slots !== null
+        }
+    }
 }
 </script>
--- client/router/index.js
+++ client/router/index.js
@@ -9,8 +9,143 @@ export default new Router
     routes:
     [
         {
-            path: '/',
+            path     : '/',
             component: require('../views/GettingStarted')
-        }
+        },
+        {
+            path     : '/elements/button',
+            component: require('../views/elements/Button')
+        },
+        {
+            path     : '/elements/container',
+            component: require('../views/elements/Container')
+        },
+        {
+            path     : '/elements/divider',
+            component: require('../views/elements/Divider')
+        },
+        {
+            path     : '/elements/header',
+            component: require('../views/elements/Header')
+        },
+        {
+            path     : '/elements/icon',
+            component: require('../views/elements/Icon')
+        },
+        {
+            path     : '/elements/image',
+            component: require('../views/elements/Image')
+        },
+        {
+            path     : '/elements/input',
+            component: require('../views/elements/Input')
+        },
+        {
+            path     : '/elements/jumbotron',
+            component: require('../views/elements/Jumbotron')
+        },
+        {
+            path     : '/elements/label',
+            component: require('../views/elements/Label')
+        },
+        {
+            path     : '/elements/list',
+            component: require('../views/elements/List')
+        },
+        {
+            path     : '/elements/loader',
+            component: require('../views/elements/Loader')
+        },
+        {
+            path     : '/elements/quote',
+            component: require('../views/elements/Quote')
+        },
+        {
+            path     : '/elements/segment',
+            component: require('../views/elements/Segment')
+        },
+        {
+            path     : '/elements/step',
+            component: require('../views/elements/Step')
+        },
+        {
+            path     : '/elements/typography',
+            component: require('../views/elements/Typography')
+        },
+        
+        {
+            path     : '/views/card',
+            component: require('../views/views/Card')
+        },
+        {
+            path     : '/views/chatroom',
+            component: require('../views/views/Chatroom')
+        },
+        {
+            path     : '/views/comment',
+            component: require('../views/views/Comment')
+        },
+        {
+            path     : '/views/feed',
+            component: require('../views/views/Feed')
+        },
+        {
+            path     : '/views/item',
+            component: require('../views/views/Item')
+        },
+        {
+            path     : '/views/statistic',
+            component: require('../views/views/Statistic')
+        },
+        
+        {
+            path     : '/collections/breadcrumb',
+            component: require('../views/collections/Breadcrumb')
+        },
+        {
+            path     : '/collections/form',
+            component: require('../views/collections/Form')
+        },
+        {
+            path     : '/collections/grid',
+            component: require('../views/collections/Grid')
+        },
+        {
+            path     : '/collections/menu',
+            component: require('../views/collections/Menu')
+        },
+        {
+            path     : '/collections/message',
+            component: require('../views/collections/Message')
+        },
+        {
+            path     : '/collections/table',
+            component: require('../views/collections/Table')
+        },
+        
+        {
+            path     : '/modules/accordion',
+            component: require('../views/modules/Accordion')
+        },
+        {
+            path     : '/modules/calendar',
+            component: require('../views/modules/Calendar')
+        },
+        {
+            path     : '/modules/checkbox',
+            component: require('../views/modules/Checkbox')
+        },
+        {
+            path     : '/modules/dimmer',
+            component: require('../views/modules/Dimmer')
+        },
+        {
+            path     : '/modules/dropdown',
+            component: require('../views/modules/Dropdown')
+        },
+        {
+            path     : '/modules/progress',
+            component: require('../views/modules/Progress')
+        },
     ]
 })
