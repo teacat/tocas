07ac7e4f28484dd8f8664a604447466552ef0621
Yami Odymel <yamiodymel@gmail.com>
2016-09-04 17:43:38 +0000

    :wrench: Donezo with the dropdown documentation

--- client/views/modules/Dropdown.vue
+++ client/views/modules/Dropdown.vue
@@ -7,6 +7,12 @@
 <script>
 import DocBasic from '../../components/DocBasic'
 import items    from '../items/modules/dropdown'
+import '../../asserts/tocas.js'
+
+setTimeout(() =>
+{
+    ts('.ts.dropdown:not(.basic)').dropdown()
+}, 1000)
 
 export default 
 {
