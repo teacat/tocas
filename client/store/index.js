2d4000affe76fbc1d01285df0c51dbe985f43b55
Yami Odymel <yamiodymel@gmail.com>
2016-09-10 13:03:21 +0000

    :wrench: Added render time action

--- client/store/index.js
+++ client/store/index.js
@@ -3,31 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const state = {
-  count: 0
+const state =
+{
+    renderTime: 0
 }
 
-const mutations = {
-  INCREMENT(state) {
-    state.count++
-  }
+const mutations =
+{
+    SET_RENDER_TIME(state, time)
+    {
+        state.renderTime = time
+    }
 }
 
-const actions = {
-  INCREMENT({commit}) {
-    commit('INCREMENT')
-  },
-  INCREMENT_ASYNC({commit}) {
-    setTimeout(() => {
-      commit('INCREMENT')
-    }, 1000)
-  }
+const actions =
+{
+    CALCULATE_RENDER_TIME({commit}, {time, startedTime})
+    {
+        commit('SET_RENDER_TIME', time - startedTime)
+    },
 }
 
-const store = new Vuex.Store({
-  state,
-  mutations,
-  actions
+const store = new Vuex.Store
+({
+    state,
+    mutations,
+    actions
 })
 
 export default store
