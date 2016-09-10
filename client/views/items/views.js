7cb0d080f938da6c57ac743acda4e991df9a9bf0
Yami Odymel <yamiodymel@gmail.com>
2016-09-10 13:02:28 +0000

    :wrench: Views page js

---
+++ client/views/items/views.js
@@ -0,0 +1,50 @@
+export default
+{
+    header:
+    {
+        title   : '視圖型',
+        subTitle: 
+        `
+            偏於外觀類的元件。
+        `
+    },
+    cards:
+    [
+        {
+            title      : '卡片',
+            class      : '.ts.card',
+            link       : '/views/card',
+            description: '用來彙整短篇文字、或是以區塊顯示碎片式內容的元素。'
+        },
+        {
+            title      : '聊天室',
+            class      : '.ts.chatroom',
+            link       : '/views/chatroom/',
+            description: '提供兩人或多人的聊天區域。'
+        },
+        {
+            title      : '留言',
+            class      : '.ts.comment',
+            link       : '/views/comment',
+            description: '用以顯示一篇文章的留言區塊。'
+        },
+        {
+            title      : '動態消息',
+            class      : '.ts.feed',
+            link       : '/views/feed/',
+            description: '顯示所有人動態消息的元件。'
+        },
+        {
+            title      : '項目',
+            class      : '.ts.item',
+            link       : '/views/item/',
+            description: '區隔項目並作為統整的元件。'
+        },
+        {
+            title      : '統計數據',
+            class      : '.ts.statistic',
+            link       : '/views/statistic/',
+            description: '用來顯示純文字的統計數據、在數字下新增標籤闡述該數值含義。'
+        },
+    ]
+}
