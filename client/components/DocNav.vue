ec38920ac9d6175dd2864632c7bed7e264de80c2
Yami Odymel <yamiodymel@gmail.com>
2016-09-10 12:31:10 +0000

    :wrench: Router links for Doc Navbar

--- client/components/DocNav.vue
+++ client/components/DocNav.vue
@@ -1,14 +1,15 @@
 <template>
     <nav class="ts large fluid borderless pure basic menu">
-        <div class="ts container">
-            <a class="item" href="#">Tocas UI</a>
-            <a class="item" href="#">入門教學</a>
-            <a class="item" href="#">一般元素</a>
-            <a class="item" href="#">聚合型</a>
-            <a class="item" href="#">模塊</a>
-            <a class="item" href="#">視圖型</a>
+        <div class="ts narrow container">
+            <router-link class="item" to="/">Tocas UI</router-link>
+            <router-link class="item" to="/getting-started/">從這開始</router-link>
+            <router-link class="item" to="/elements/">一般元素</router-link>
+            <router-link class="item" to="/collections/">聚合型</router-link>
+            <router-link class="item" to="/modules/">模塊</router-link>
+            <router-link class="item" to="/views/">視圖型</router-link>
             <div class="right menu">
-                <a class="item" href="#">Github</a>
+                <a class="item" href="https://patreon.com/yamiodymel">贊助</a>
+                <a class="item" href="https://github.com/TeaMeow/TocasUI/">Github</a>
             </div>
         </div>
     </nav>
