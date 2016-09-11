c74aefda1d8696e3eeb3d2b7b738f09c1e286d72
Yami Odymel <yamiodymel@gmail.com>
2016-09-11 06:07:35 +0000

    :wrench: Added example page and empty examples

---
+++ client/views/Examples.vue
@@ -0,0 +1,26 @@
+<template>
+    <doc-list :cards="cards.cards" 
+              :title="cards.header.title"
+              :subTitle="cards.header.subTitle"></doc-list>
+</template>
+
+<script>
+import DocList from '../components/DocList'
+import cards   from './items/examples'
+
+export default 
+{
+    components: 
+    {
+        DocList
+    },
+    data()
+    {
+        return { cards: cards }        
+    },
+    beforeCreate()
+    {
+        document.title = '範例 | Tocas UI'
+    }
+}
+</script>
