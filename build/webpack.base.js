35f17d141e6d271e5266f4990b823cad9a1c4eb9
Yami Odymel <yamiodymel@gmail.com>
2016-09-17 08:14:06 +0000

    http:// to https://

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -47,7 +47,7 @@ module.exports =
                 query :
                 {
                   limit: 10000,
-                  name: '/img/[hash:7].[ext]'
+                  name: 'img/[hash:7].[ext]'
                 }
             },
             {
