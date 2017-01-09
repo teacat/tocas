b4596c9a0e110b8efc651f6ed459e741ba7f23a3
Yami Odymel <yamiodymel@gmail.com>
2017-01-09 23:25:50 +0800

    Fixed #157, #167, #170

--- src/tocas.js
+++ src/tocas.js
@@ -711,3 +711,49 @@ ts.fn.find = function(selector)
 
             return isTrue;
         }
+
+        /**
+ * Attr
+ * 
+ * Add a attribute to an element.
+ * 
+ * @param string attr    The name of the attribute.
+ * @param mixed  value   The value of the attribute.
+ * 
+ * @return mixed
+ */
+ 
+ts.fn.attr = function(attr, value)
+{
+    value = (value === null) ? null : value;
+    
+    /** Set multiple Attr if Attr is object */
+    if(typeof attr === 'object' && !value)
+        return this.each(function(){ for(var i in attr) this.setAttribute(i, attr[i]); });
+    
+    /** Set single attr */
+    else if(attr != null && value != null)
+        return this.each(function(){ this.setAttribute(attr, value); });
+
+    /** Get single attr only if Attr is not null */
+    else if(attr != null && !value)
+        return 0 in this ? this[0].getAttribute(attr) : null;
+}
+
+
+
+
+/**
+ * Remove Attr
+ * 
+ * Remove an attribute from an element.
+ * 
+ * @param string attr   The name of the attribute.
+ * 
+ * @return object
+ */
+ 
+ts.fn.removeAttr = function(attr)
+{
+    return this.each(function(){ this.removeAttribute(attr); });
+}
