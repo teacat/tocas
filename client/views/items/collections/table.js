9cf11db7ca855a6f86097125b412dc1f27f15b00
Yami Odymel <yamiodymel@gmail.com>
2017-01-14 02:36:30 +0800

    Added sortable table

--- client/views/items/collections/table.js
+++ client/views/items/collections/table.js
@@ -926,6 +926,61 @@ export default
         },
         {
             type       : small,
+            title      : '可排序',
+            description: `使用者可以透過點擊欄位標頭來重新排序表格。排序的方式以系統語系設定為主，英文會以 A-Z 與 Z-A 作為排列，中文則是筆畫順序，俄文則是字母排序。`,
+            link       : 'sortable'
+        },
+        {
+            type: 'example',
+            code: `<table class="ts sortable table">
+    <thead>
+        <tr>
+            <th>檔案名稱</th>
+            <th>中文分類</th>
+            <th>說明</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>endpoints.go</td>
+            <td>終端點</td>
+            <td>轉繼站會將收到的資訊轉換成終端點並進行下一個操作。</td>
+        </tr>
+        <tr>
+            <td>instrumenting.go</td>
+            <td>度量</td>
+            <td>將微服務中的函式處理時間整理成一定的格式並且傳送給 Prometheus 伺服器做統計視覺化整理。</td>
+        </tr>
+        <tr>
+            <td>logging.go</td>
+            <td>紀錄</td>
+            <td>觀察並且記錄微服務中所有函式的呼叫與流向。</td>
+        </tr>
+        <tr>
+            <td>main.go</td>
+            <td>主要</td>
+            <td>核心檔案，除了開發者外不應由其他人修改。</td>
+        </tr>
+        <tr>
+            <td>service.go</td>
+            <td>服務函式</td>
+            <td>核心服務的主要功能。</td>
+        </tr>
+    </tbody>
+</table>`,
+            mark: 'sortable'
+        },
+        {
+            type       : tiny,
+            title      : 'JavaScript',
+            description: '為此，你需要透過下列程式碼來驅動可排序表格。',
+        },
+        {
+            type: source,
+            code: `ts('.ts.sortable.table').tablesort()`
+        },
+        {
+            type       : small,
             title      : '基本',
             description: `表格可以只剩下基本的構造線條。`,
             link       : 'basic'
