de2be544a8de6af51a2957590826727b68c473ef
Yami Odymel <yamiodymel@gmail.com>
2017-01-14 02:35:25 +0800

    Added symbols for list cards

--- client/views/items/modules.js
+++ client/views/items/modules.js
@@ -14,49 +14,64 @@ export default
             title      : '手風琴',
             class      : '.ts.accordion',
             link       : '/modules/accordion',
-            description: '一個可供伸展的內容區塊。'
+            description: '一個可供伸展的內容區塊。',
+            symbol     : 'resize vertical'
         },
         {
             title      : '日曆',
             class      : '.ts.calendar',
             link       : '/modules/calendar/',
-            description: '用以表示日期的元件。'
+            description: '用以表示日期的元件。',
+            symbol     : 'calendar'
         },
         {
             title      : '核取方塊',
             class      : '.ts.checkbox',
             link       : '/modules/checkbox',
-            description: '讓使用者進行勾取、選取動作的核取方塊。'
+            description: '讓使用者進行勾取、選取動作的核取方塊。',
+            symbol     : 'checkmark box'
         },
         {
             title      : '淡化幕',
             class      : '.ts.dimmer',
             link       : '/modules/dimmer/',
-            description: '新增一層暗幕用以遮蔽特定元素。'
+            description: '新增一層暗幕用以遮蔽特定元素。',
+            symbol     : 'hide'
         },
         {
             title      : '下拉式選單',
             class      : '.ts.dropdown',
             link       : '/modules/dropdown/',
-            description: '下拉式的表單輸入欄位和功能選單。'
+            description: '下拉式的表單輸入欄位和功能選單。',
+            symbol     : 'caret down'
         },
         {
             title      : '進度列',
             class      : '.ts.progress',
             link       : '/modules/progress/',
-            description: '用以呈現進度或者是一個總體配額。'
+            description: '用以呈現進度或者是一個總體配額。',
+            symbol     : 'tasks'
         },
         {
             title      : '滑桿',
             class      : '.ts.slider',
             link       : '/modules/slider/',
-            description: '以滑桿來取代輸入欄位設定數值。'
+            description: '以滑桿來取代輸入欄位設定數值。',
+            symbol     : 'sliders'
         },
         {
             title      : '彈出式訊息',
             class      : '.ts.popup',
             link       : '/modules/popup/',
-            description: '滑鼠移過時會彈出一個訊息。'
+            description: '滑鼠移過時會彈出一個訊息。',
+            symbol     : 'talk outline'
+        },
+        {
+            title      : '對話視窗',
+            class      : '.ts.modal',
+            link       : '/modules/modal/',
+            description: '跳出一個對話、表單視窗並阻擋背後的其他行為，令使用者專注在跳出的視窗。',
+            symbol     : 'window maximize'
         },
     ]
 }
--- client/views/items/views.js
+++ client/views/items/views.js
@@ -14,37 +14,43 @@ export default
             title      : '卡片',
             class      : '.ts.card',
             link       : '/views/card',
-            description: '用來彙整短篇文字、或是以區塊顯示碎片式內容的元素。'
+            description: '用來彙整短篇文字、或是以區塊顯示碎片式內容的元素。',
+            symbol     : 'identity card'
         },
         {
             title      : '聊天室',
             class      : '.ts.chatroom',
             link       : '/views/chatroom/',
-            description: '提供兩人或多人的聊天區域。'
+            description: '提供兩人或多人的聊天區域。',
+            symbol     : 'users'
         },
         {
             title      : '留言',
             class      : '.ts.comment',
             link       : '/views/comment',
-            description: '用以顯示一篇文章的留言區塊。'
+            description: '用以顯示一篇文章的留言區塊。',
+            symbol     : 'comments outline'
         },
         {
             title      : '動態消息',
             class      : '.ts.feed',
             link       : '/views/feed/',
-            description: '顯示所有人動態消息的元件。'
+            description: '顯示所有人動態消息的元件。',
+            symbol     : 'rss'
         },
         {
             title      : '項目',
             class      : '.ts.item',
             link       : '/views/item/',
-            description: '區隔項目並作為統整的元件。'
+            description: '區隔項目並作為統整的元件。',
+            symbol     : 'th list'
         },
         {
             title      : '統計數據',
             class      : '.ts.statistic',
             link       : '/views/statistic/',
-            description: '用來顯示純文字的統計數據、在數字下新增標籤闡述該數值含義。'
+            description: '用來顯示純文字的統計數據、在數字下新增標籤闡述該數值含義。',
+            symbol     : 'bar chart'
         },
     ]
 }
