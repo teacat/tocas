3e95a5fe4d4309c5edd4e84a9987345afcf52fc5
Yami Odymel <yamiodymel@gmail.com>
2017-03-19 01:22:26 +0800

    Fixed card symbol div

--- client/components/DocCards.vue
+++ client/components/DocCards.vue
@@ -8,7 +8,7 @@
                 </div>
                 <div class="description" v-text="card.description"></div>
             </div>
-            <div class="symbol"><i class="icon" :class="card.symbol"></i>
+            <div class="symbol"><i class="icon" :class="card.symbol"></i></div>
         </router-link>
     </article>
 </template>
