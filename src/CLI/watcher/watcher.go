96dd599c58773dc84b88f2174739be3ff846d564
YamiOdymel <yamiodymel@gmail.com>
2018-12-04 16:37:03 +0800

    Watch the System file changes

--- src/CLI/watcher/watcher.go
+++ src/CLI/watcher/watcher.go
@@ -58,6 +58,9 @@ func (w *Watcher) Run() {
 	if err := w.Watcher.AddRecursive("../Docs"); err != nil {
 		log.Fatalln(err)
 	}
+	if err := w.Watcher.AddRecursive("../System"); err != nil {
+		log.Fatalln(err)
+	}
 	if err := w.Watcher.Start(time.Millisecond * 100); err != nil {
 		log.Fatalln(err)
 	}
