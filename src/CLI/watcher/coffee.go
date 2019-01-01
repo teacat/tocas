a092fb326b2de6ce7c1ac600c5619503a58a97af
YamiOdymel <yamiodymel@gmail.com>
2019-01-02 01:41:38 +0800

    Coffee compiler with bare option

--- src/CLI/watcher/coffee.go
+++ src/CLI/watcher/coffee.go
@@ -13,5 +13,5 @@ import (
 func (w *Watcher) CoffeeHandler(event watcher.Event) {
 	dest := strings.ToLower(strings.TrimSuffix(filepath.Base(event.Path), filepath.Ext(filepath.Base(event.Path))))
 
-	w.Executor.ExecuteWithEvent(event, []string{"coffee", "--output", fmt.Sprintf("%s%s.js", path.DistPath, dest), "--compile", event.Path})
+	w.Executor.ExecuteWithEvent(event, []string{"coffee", "-b", "--output", fmt.Sprintf("%s%s.js", path.DistPath, dest), "--compile", event.Path})
 }
