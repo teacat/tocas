a06c56a9875c36db8c644305f204ec09230f035c
YamiOdymel <yamiodymel@gmail.com>
2020-12-02 14:30:22 +0800

    不同的 Shades color generator

--- cli/color-generator/main.go
+++ cli/color-generator/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"math"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/teacat/noire"
 	"github.com/urfave/cli"
@@ -13,8 +15,15 @@ import (
 var colorWeights = []int{
 	50, 75, 100, 200, 300, 400, 500, 600, 700, 800, 900,
 }
-var colorShades = []float64{
+
+var colorShadesGray = []float64{
 	-1, -0.93, -0.87, -0.72, -0.6, -0.3, 0, 0.205, 0.385, 0.58, 0.755,
+}
+var colorShadesLight = []float64{
+	-1, -0.93, -0.87, -0.72, -0.6, -0.3, 0, 0.06, 0.205, 0.385, 0.58,
+}
+var colorShadesDark = []float64{
+	-1, -0.93, -0.87, -0.72, -0.6, -0.3, 0, 0.1, 0.205, 0.385, 0.58,
 }
 
 func main() {
@@ -30,7 +39,13 @@ func main() {
 				Name:    "color",
 				Aliases: []string{"c"},
 				Value:   "",
-				Usage:   "base color, without #, ex: 00ADEA, FFF",
+				Usage:   "base color, hex or r,g,b, ex: #00ADEA or 255, 255, 255",
+			},
+			&cli.StringFlag{
+				Name:    "shades",
+				Aliases: []string{"s"},
+				Value:   "dark",
+				Usage:   "shades level for: gray, light, dark",
 			},
 		},
 		Name:   "color-generator",
@@ -43,14 +58,38 @@ func main() {
 }
 
 func endpoint(c *cli.Context) error {
+	var shades []float64
+	//
+	switch c.String("shades") {
+	case "gray":
+		shades = colorShadesGray
+	case "light":
+		shades = colorShadesLight
+	case "dark":
+		shades = colorShadesDark
+	}
+
+	//
 	for k, w := range colorWeights {
-		color := noire.NewHex(c.String("color"))
-		shade := colorShades[k]
+		//
+		var color noire.Color
+		if string(c.String("color")[0]) == "#" {
+			color = noire.NewHex(c.String("color"))
+		} else {
+			rgb := strings.Split(c.String("color"), ",")
+			r, _ := strconv.Atoi(strings.TrimSpace(rgb[0]))
+			g, _ := strconv.Atoi(strings.TrimSpace(rgb[1]))
+			b, _ := strconv.Atoi(strings.TrimSpace(rgb[2]))
+			color = noire.NewRGB(float64(r), float64(g), float64(b))
+		}
+		//
+		shade := shades[k]
 		if shade < 0 {
 			color = color.Tint(math.Abs(shade))
 		} else if shade > 0 {
 			color = color.Shade(math.Abs(shade))
 		}
+		//
 		fmt.Printf("--ts-global_%s-%d: %.0f, %.0f, %.0f;\n", c.String("name"), w, color.Red, color.Green, color.Blue)
 	}
 	return nil
