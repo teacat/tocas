102adbef569813446dea8746bb5d6dd7020ba75e
YamiOdymel <yamiodymel@gmail.com>
2020-12-24 16:44:40 +0800

    新增 article tool bar menu

--- cli/docs-builder/templates/assets/scripts/article.js
+++ cli/docs-builder/templates/assets/scripts/article.js
@@ -23,6 +23,37 @@ document.addEventListener("DOMContentLoaded", () => {
         document.documentElement.scrollTop = 0;
     });
 
+    document.addEventListener("click", (e) => {
+
+        document.querySelectorAll(".文件內容-主要欄位-工具列-項目-下拉式選單").forEach((e) => {
+            e.querySelector(".文件內容-主要欄位-工具列-項目-下拉式選單-群組").classList.remove("文件內容-主要欄位-工具列-項目-下拉式選單-群組_開啟的")
+        })
+    })
+
+    document.querySelectorAll(".文件內容-主要欄位-工具列-項目-下拉式選單").forEach((e) => {
+        e.addEventListener("click", (e) => {
+            var menu = e.target.closest(".文件內容-主要欄位-工具列-項目-下拉式選單")
+
+            if (e.target.classList.contains("文件內容-主要欄位-工具列-項目-下拉式選單-群組-項目")) {
+                let [key, value] = e.target.getAttribute("data-value").split(",")
+                document.querySelector("html").setAttribute(`data-${key}`, value)
+                menu.querySelectorAll(".文件內容-主要欄位-工具列-項目-下拉式選單-群組-項目").forEach((e) => {
+                    e.classList.remove("文件內容-主要欄位-工具列-項目-下拉式選單-群組-項目_啟用的")
+                })
+                menu.querySelector(".文件內容-主要欄位-工具列-項目-下拉式選單-文字-標籤").innerText = e.target.innerText
+                e.target.classList.add("文件內容-主要欄位-工具列-項目-下拉式選單-群組-項目_啟用的")
+            } else {
+                setTimeout(() => {
+                    menu.
+                        querySelector(".文件內容-主要欄位-工具列-項目-下拉式選單-群組").
+                        classList.add("文件內容-主要欄位-工具列-項目-下拉式選單-群組_開啟的")
+                }, 1)
+            }
+
+
+        });
+     })
+
     window.addEventListener("hashchange", rescanAnchor);
 
     rescanAnchor();
