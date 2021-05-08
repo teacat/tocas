c9122f57cc97b98f8ecf7a71b6920b4b13c51e46
YamiOdymel <yamiodymel@gmail.com>
2021-05-08 19:30:48 +0800

    Back to Svelte, Wow

---
+++ srcx/playground/Accordion.vue
@@ -0,0 +1,79 @@
+<script>
+import Accordion from './../components/Accordion.vue'
+import AccordionItem from './../components/AccordionItem.vue'
+
+export default {
+    name: 'AccordionPlayground',
+    components: {
+        Accordion,
+        AccordionItem,
+    }
+}
+</script>
+
+<template>
+    <div id="app">
+        <!-- Default -->
+        <h1>Default</h1>
+        <Accordion>
+            <AccordionItem>
+                <div slot="header">Header 1</div>
+                <div slot="description">Description 1</div>
+                <div slot="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </AccordionItem>
+            <AccordionItem>
+                <div slot="header">Header 2</div>
+                <div slot="description">Description 2</div>
+                <div slot="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </AccordionItem>
+        </Accordion>
+
+        <!-- Multiple -->
+        <h1>Multiple</h1>
+        <Accordion multiple>
+            <AccordionItem>
+                <div slot="header">Header 1</div>
+                <div slot="description">Description 1</div>
+                <div slot="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </AccordionItem>
+            <AccordionItem>
+                <div slot="header">Header 2</div>
+                <div slot="description">Description 2</div>
+                <div slot="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </AccordionItem>
+        </Accordion>
+
+        <!-- Expanded -->
+        <h1>Expanded</h1>
+        <Accordion>
+            <AccordionItem>
+                <div slot="header">Header 1</div>
+                <div slot="description">Description 1</div>
+                <div slot="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </AccordionItem>
+            <AccordionItem expanded>
+                <div slot="header">Header 2</div>
+                <div slot="description">Description 2</div>
+                <div slot="content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </AccordionItem>
+        </Accordion>
+    </div>
+</template>
--- svelte/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Divider       from "./Divider/Divider.svelte"
-import Icon          from "./Icon/Icon.svelte"
-import Accordion     from "./Accordion/Accordion.svelte"
-import AccordionItem from "./Accordion/AccordionItem.svelte"
