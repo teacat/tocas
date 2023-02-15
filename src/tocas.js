ed603aa38fe5039b572c48b41dbe3744758d6701
YamiOdymel <yamiodymel@gmail.com>
2023-02-16 01:55:19 +0800

    Debugging new Tocas JS

--- src/tocas.js
+++ src/tocas.js
@@ -1,19 +1,20 @@
+/**
+ * Responsive
+ */
+
 (function () {
     /**
+     * breakpoint, breakpoint+, breakpoint-, breakpoint-breakpoint
+     * [size]+, [size]-, [minSize-maxSize]
      *
-     * breakpoint
-     * breakpoint+
-     * breakpoint-
-     * breakpoint-breakpoint
-     * [size]+
-     * [size]-
-     * [minSize-maxSize]
      * @breakpoint, @breakpoint+, @breakpoint-, @breakpoint-breakpoint
      * @[size]+, @[size]-, @[minSize-maxSize]
      */
 
     var selector_container = `[class~="@container"]`;
     var selector_responsive = `[class*=":is-"],[class*=":u-"]`;
+    var selector_toggle_class = `[data-toggle="class"]`;
+    var selector_toggle_dropdown = `[data-toggle="dropdown"]`;
 
     // parseUnit
     function parseUnit(value) {
@@ -96,7 +97,6 @@
     }
 
     function check(element, container_rect) {
-
         //
         element.classList.forEach(class_name => {
             //
@@ -111,7 +111,7 @@
             //
             if (isContainerQuery || container_rect !== undefined) {
                 if (container_rect !== undefined) {
-                    width = container_rect.width
+                    width = container_rect.width;
                 } else {
                     var container = element.closest(selector_container);
                     if (container === null) {
@@ -136,66 +136,231 @@
         });
     }
 
-    document.addEventListener("DOMContentLoaded", () => {
-        const resize_observer = new ResizeObserver(entries => {
-            entries.forEach(entry => {
-                // @breakpoint:is-class
-                entry.target.querySelectorAll(`[class^="@"]:is([class*=":is-"],[class*=":u-"])`).forEach(element => {
-                    check(element);
-                });
+    const resize_observer = new ResizeObserver(entries => {
+        entries.forEach(entry => {
+            // @breakpoint:is-class
+            entry.target.querySelectorAll(`[class^="@"]:is([class*=":is-"],[class*=":u-"])`).forEach(element => {
+                check(element);
             });
         });
+    });
 
-        document.querySelectorAll(selector_container).forEach(element => {
-            resize_observer.observe(element);
+    document.querySelectorAll(selector_container).forEach(element => {
+        resize_observer.observe(element);
+    });
+
+    // toggleClass
+    function toggleClass(event) {
+        var element = event.target.closest("[data-toggle]");
+
+        var target = element.getAttribute("data-toggle");
+        var [selector, class_name] = target.split(":");
+
+        document.querySelectorAll(selector).forEach(v => {
+            v.classList.toggle(class_name);
         });
+    }
 
-        const mutation_observer = new MutationObserver(function (mutations) {
-            mutations.forEach(function (mutation) {
-                console.log(mutation)
+    // bindToggle
+    function bindToggle(element) {
+        element.removeEventListener("click", toggleClass);
+        element.addEventListener("click", toggleClass);
+    }
 
-                //
-                if (mutation.type === "attributes") {
-                    check(mutation.target);
+    function toggleDropdown(event) {
+        var element = event.target.closest(selector_toggle_dropdown);
+        var dropdown = element.parentNode.querySelector(".ts-dropdown");
 
-                    if (mutation.target.matches(selector_container)) {
-                        resize_observer.observe(mutation.target);
-                    }
-                } else if (mutation.addedNodes && mutation.addedNodes.length) {
-                    mutation.addedNodes.forEach(function (addedNode) {
-                        if (addedNode.nodeType !== Node.ELEMENT_NODE) {
+        dropdown.classList.toggle("is-visible");
+    }
+
+    function bindDropdown(element) {
+        element.removeEventListener("click", toggleDropdown);
+        element.addEventListener("click", toggleDropdown);
+    }
+
+    const mutation_observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            //
+            if (mutation.type === "attributes") {
+                check(mutation.target);
+
+                if (mutation.target.matches(selector_container)) {
+                    resize_observer.observe(mutation.target);
+                }
+
+                if (mutation.attributeName === "class") {
+                    if (mutation.target.matches(".ts-dropdown.is-visible")) {
+                        if (mutation.oldValue.includes("is-visible")) {
                             return;
                         }
-                        if (addedNode.matches(selector_responsive)) {
-                            check(addedNode);
+                        if (mutation.target.parentNode.querySelector(selector_toggle_dropdown) === null) {
+                            return;
                         }
-                        if (addedNode.matches(selector_container)) {
-                            resize_observer.observe(addedNode);
+                        mutation.target.dropdown = window.FloatingUIDOM.autoUpdate(mutation.target.parentNode.querySelector(selector_toggle_dropdown), mutation.target, () => {
+                            window.FloatingUIDOM.computePosition(mutation.target.parentNode.querySelector(selector_toggle_dropdown), mutation.target, {
+                                placement: "bottom-end",
+                                middleware: [window.FloatingUIDOM.flip(), window.FloatingUIDOM.offset(10)],
+                            }).then(({ x, y }) => {
+                                Object.assign(mutation.target.style, {
+                                    left: `${x}px`,
+                                    top: `${y}px`,
+                                });
+                            });
+                        });
+                    }
+
+                    if (mutation.target.matches(".ts-dropdown:not(.is-visible)")) {
+                        if (!mutation.oldValue.includes("is-visible")) {
+                            return;
                         }
-                    });
+                        mutation.target.dropdown();
+                        mutation.target.dropdown = undefined;
+                    }
                 }
-            });
-        });
+            } else if (mutation.addedNodes && mutation.addedNodes.length) {
+                mutation.addedNodes.forEach(function (addedNode) {
+                    if (addedNode.nodeType !== Node.ELEMENT_NODE) {
+                        return;
+                    }
+                    if (addedNode.matches(selector_responsive)) {
+                        check(addedNode);
+                    }
+                    if (addedNode.matches("[data-toggle]")) {
+                        bindToggle(addedNode);
+                    }
+                    if (addedNode.matches(selector_toggle_dropdown)) {
+                        bindDropdown(addedNode);
+                    }
 
-        mutation_observer.observe(document.documentElement, {
-            childList: true,
-            subtree: true,
-            attributes: true,
-            attributeOldValue: true,
-            attributeFilter: ["class"],
-        });
+                    if (addedNode.matches("[data-tooltip]")) {
+                        (function () {
+                            var timer = null;
 
-        document.querySelectorAll(selector_responsive).forEach(element => {
-            check(element);
+                            addedNode.addEventListener("mouseover", () => {
+                                timer = setTimeout(() => {
+                                    var tooltip = document.createElement("div");
+                                    tooltip.innerText = addedNode.getAttribute("data-tooltip");
+                                    tooltip.classList.add("ts-tooltip", "is-visible")
+
+                                    var arrow = document.createElement("div");
+                                    arrow.classList.add("arrow")
+
+                                    tooltip.appendChild(arrow)
+
+                                    document.body.appendChild(tooltip);
+
+                                    addedNode.tooltip = window.FloatingUIDOM.autoUpdate(addedNode, tooltip, () => {
+                                        window.FloatingUIDOM.computePosition(addedNode, tooltip, {
+
+                                            strategy: "fixed",
+                                            middleware: [window.FloatingUIDOM.autoPlacement({
+                                                // 'right' and 'left' won't be chosen
+                                                allowedPlacements: ['top', 'bottom'],
+                                              }), window.FloatingUIDOM.offset({
+                                                crossAxis: -15,
+                                                mainAxis: 10,
+                                            }), window.FloatingUIDOM.shift(), window.FloatingUIDOM.arrow({
+                                                element: arrow,
+                                              })],
+                                        }).then(({middlewareData, x, y, placement}) => {
+                                            Object.assign(tooltip.style, {
+                                                left: `${x}px`,
+                                                top: `${y}px`,
+                                            });
+
+                                            if (middlewareData.arrow) {
+                                                const {x, y} = middlewareData.arrow;
+
+                                                Object.assign(arrow.style, {
+                                                  left: x != null ? `${x}px` : '',
+                                                  top: y != null ? `${y}px` : '',
+                                                });
+                                            }
+
+                                            if (placement) {
+                                                if (placement === 'top') {
+                                                    tooltip.classList.add("is-top")
+                                                }
+                                                if (placement === 'bottom') {
+                                                    tooltip.classList.add("is-bottom")
+                                                }
+                                            }
+                                        });
+                                    });
+                                }, 200);
+                            });
+                            // check leave tooltip
+                            addedNode.addEventListener("mouseleave", () => {
+                                if (addedNode.tooltip !== undefined) {
+                                    addedNode.tooltip();
+                                    addedNode.tooltip = undefined;
+                                }
+                                document.querySelectorAll(".ts-tooltip.is-visible").forEach((v) => {
+                                    v.remove();
+                                })
+                                if (timer !== null) {
+                                    clearTimeout(timer);
+                                    timer = null;
+                                }
+                            });
+                        })();
+                    }
+
+                    if (addedNode.matches(selector_container)) {
+                        resize_observer.observe(addedNode);
+                    }
+                });
+            }
         });
     });
 
 
-
+    mutation_observer.observe(document.documentElement, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+        attributeOldValue: true,
+        attributeFilter: ["class"],
+    });
 
     window.addEventListener("resize", event => {
         document.querySelectorAll(selector_responsive).forEach(element => {
             check(element);
         });
     });
+})();
+
+/**
+ * Tab
+ */
+
+(function () {
+
+})();
+
+
+
+/**
+ * Toggle
+ */
+
+(function () {
+
+})();
+
+/**
+ * Dropdown
+ */
+
+(function () {
+
+})();
+
+/**
+ * Tooltip
+ */
+
+(function () {
+
 })();
