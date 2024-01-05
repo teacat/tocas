65408aa1b56282cce06fb973d781dbf31ac01156
Yami Odymel <yamiodymel@gmail.com>
2024-01-05 14:44:53 +0800

    Clean dropdown js, tooltip back with popover

--- src/scripts/tocas.dropdown.js
+++ src/scripts/tocas.dropdown.js
@@ -1,179 +1,179 @@
 class Dropdown {
     // #dropdowns 用以隨時更新頁面上有哪些存在的彈出式選單 ID，
     // 這個清單資料來自於有被指定在 [data-dropdown] 裡的名稱。
-    #dropdowns = new Set();
+    #dropdowns = new Set()
 
     // attributeMutation
-    attributeMutation = mutation => {};
+    attributeMutation = mutation => {}
 
     // addedNodeMutation
     addedNodeMutation = added_node => {
         // 當有新的節點被增加，就更新彈出式選單的清單。
-        this.updateDropdowns();
+        this.updateDropdowns()
 
         // 如果這個追加的 DOM 元素是一個會觸發彈出式選單的元素，就監聽其點擊事件。
         if (this.isDropdownTrigger(added_node)) {
-            this.bindEventListener(added_node);
+            this.bindEventListener(added_node)
         }
 
         // 如果這個追加的 DOM 元素是一個彈出式選單容器，就監聽其選項點擊事件。
         if (this.isDropdown(added_node)) {
-            this.bindItemEventListener(added_node);
+            this.bindItemEventListener(added_node)
         }
-    };
+    }
 
     // isDropdownTrigger
     isDropdownTrigger = element => {
-        return element.matches("[data-dropdown]");
-    };
+        return element.matches("[data-dropdown]")
+    }
 
     // isDropdown
     isDropdown = element => {
         // 必須要有 .ts-dropdown 且 ID 有出現在其他元素的 data-dropdown 屬性裡面。
-        return element.matches(`.ts-dropdown`) && this.#dropdowns.has(element.id);
-    };
+        return element.matches(`.ts-dropdown`) && this.#dropdowns.has(element.id)
+    }
 
     // updateDropdowns
     updateDropdowns = () => {
         document.querySelectorAll("[data-dropdown]").forEach(element => {
-            this.#dropdowns.add(element.dataset.dropdown);
-        });
+            this.#dropdowns.add(element.dataset.dropdown)
+        })
     }
 
     // position
     position = element => {
-        return element.dataset.position || "bottom-start";
-    };
+        return element.dataset.position || "bottom-start"
+    }
 
     // windowClick
     windowClick = event => {
         // 取得這個視窗點擊最鄰近的 Dropdown 模組觸發元素。
-        var closest_trigger = event.target.closest("[data-dropdown]");
+        var closest_trigger = event.target.closest("[data-dropdown]")
 
         // 取得這個視窗點擊最鄰近的 Dropdown 容器本身。
-        var closest_dropdown = event.target.closest(".ts-dropdown");
+        var closest_dropdown = event.target.closest(".ts-dropdown")
 
         // 如果這個點擊事件既沒有關聯任何觸發元素，也沒有在點擊任何 Dropdown 容器，
         // 那使用者應該就是在點擊其他東西，所以關閉所有頁面上可見的彈出式選單。
         if (closest_trigger === null && closest_dropdown === null) {
             document.querySelectorAll(".ts-dropdown").forEach(dropdown => {
-                this.closeDropdown(dropdown);
-            });
+                this.closeDropdown(dropdown)
+            })
         }
 
         // 如果這個點擊事件是在點擊一個會開關 Dropdown 的觸發元素。
         if (closest_trigger !== null) {
             // 取得這個觸發元素原本會打開的 Dropdown 名稱。
-            var name = closest_trigger.dataset.dropdown;
+            var name = closest_trigger.dataset.dropdown
 
             // 透過該名稱搜尋對應的 Dropdown。
             var dropdown = document.getElementById(name)
 
             // 除了找到的這個對應 Dropdown 以外，關掉其他所有 Dropdown。
-            this.closeDropdownsExcept(dropdown);
+            this.closeDropdownsExcept(dropdown)
         }
 
         // 如果這個點擊事件是在點擊某個 Dropdown 容器或內部的項目。
         if (closest_dropdown !== null) {
             // 關閉這個 Dropdown 以外的其他所有 Dropdown。
-            this.closeDropdownsExcept(closest_dropdown);
+            this.closeDropdownsExcept(closest_dropdown)
         }
-    };
+    }
 
     // closeDropdownsExcept
     closeDropdownsExcept = excluded_dropdown => {
         document.querySelectorAll(".ts-dropdown").forEach(dropdown => {
             if (dropdown !== excluded_dropdown) {
-                this.closeDropdown(dropdown);
+                this.closeDropdown(dropdown)
             }
-        });
-    };
+        })
+    }
 
     // bindEventListener
     bindEventListener = element => {
-        element.removeEventListener("click", this.clickEventListener);
-        element.addEventListener("click", this.clickEventListener);
-    };
+        element.removeEventListener("click", this.clickEventListener)
+        element.addEventListener("click", this.clickEventListener)
+    }
 
     // bindItemEventListener
     bindItemEventListener = element => {
-        element.removeEventListener("click", this.itemClickEventListener);
-        element.addEventListener("click", this.itemClickEventListener);
-    };
+        element.removeEventListener("click", this.itemClickEventListener)
+        element.addEventListener("click", this.itemClickEventListener)
+    }
 
     // closeDropdown
     closeDropdown = dropdown => {
         // 如果這個元素不包含 `ts-dropdown` 或者也不是可見狀態，就忽略不計。
         if (!dropdown.classList.contains(".ts-dropdown") && !dropdown.classList.contains("is-visible")) {
-            return;
+            return
         }
 
         // 如果這個選單不在清單裡，就不要在乎是否該關閉這個選單，
         // 因為這很有可能是 .ts-dropdown 但由使用者自行控制可見狀態。
         if (!this.#dropdowns.has(dropdown.id)) {
-            return;
+            return
         }
 
         // 移除這個彈出式選單的可見狀態。
-        dropdown.classList.remove("is-visible");
+        dropdown.classList.remove("is-visible")
 
         // 如果這個彈出式選單有 FLoating UI 的清除函式，就呼叫該清除函式，
         // 然後重設對應的 CSS 變數。
         if (dropdown.tocas_dropdown !== undefined) {
-            dropdown.tocas_dropdown();
-            dropdown.tocas_dropdown = undefined;
-            dropdown.style.removeProperty("--ts-dropdown-min-width");
-            dropdown.style.removeProperty("--ts-dropdown-position");
+            dropdown.tocas_dropdown()
+            dropdown.tocas_dropdown = undefined
+            dropdown.style.removeProperty("--ts-dropdown-min-width")
+            dropdown.style.removeProperty("--ts-dropdown-position")
         }
-    };
+    }
 
     // itemClickEventListener
     itemClickEventListener = event => {
         // 取得這個點擊事件最鄰近的彈出式選單。
-        var dropdown = event.target.closest(".ts-dropdown");
+        var dropdown = event.target.closest(".ts-dropdown")
 
         // 如果找不到點擊事件最鄰近的選單項目，
         // 那可能點擊的不是項目而是其他容器裡的東西，那就忽略這個動作。
         if (event.target.closest(".item") === null) {
-            return;
+            return
         }
 
         // 項目點擊成功，關閉這個彈出式選單。
-        this.closeDropdown(dropdown);
-    };
+        this.closeDropdown(dropdown)
+    }
 
     // clickEventListener
     clickEventListener = event => {
-        var element = event.target.closest("[data-dropdown]");
+        var element = event.target.closest("[data-dropdown]")
 
         // 取得這個觸發元素會切換的彈出式選單名稱。
-        var name = element.dataset.dropdown;
+        var name = element.dataset.dropdown
 
         // 透過命名空間搜尋對應的彈出式選單。
         var target = document.getElementById(name)
 
         // 取得目標選單的偏好位置設定。
-        var position = this.position(target);
+        var position = this.position(target)
 
         // 如果那個選單有 Floating UI 清除函式，就先清除並且重設相關位置設定。
         if (target.tocas_dropdown !== undefined) {
-            target.tocas_dropdown();
-            target.tocas_dropdown = undefined;
-            target.style.removeProperty("--ts-dropdown-min-width");
-            target.style.removeProperty("--ts-dropdown-position");
+            target.tocas_dropdown()
+            target.tocas_dropdown = undefined
+            target.style.removeProperty("--ts-dropdown-min-width")
+            target.style.removeProperty("--ts-dropdown-position")
         }
 
         // 切換目標彈出式選單的可見度。
-        target.classList.toggle("is-visible");
+        target.classList.toggle("is-visible")
 
         // 如果目標選單現在不再可見，就是被隱藏了，那就不需要執行接下來的行為。
         if (!target.classList.contains("is-visible")) {
-            return;
+            return
         }
 
         // 設定選單的最小寬度和絕對位置，至少要跟切換觸發元素一樣寬。
-        target.style.setProperty("--ts-dropdown-min-width", `${element.getBoundingClientRect().width}px`);
-        target.style.setProperty("--ts-dropdown-position", "fixed");
+        target.style.setProperty("--ts-dropdown-min-width", `${element.getBoundingClientRect().width}px`)
+        target.style.setProperty("--ts-dropdown-position", "fixed")
 
         // 透過 Floating UI 來觸發浮動顯示。
         target.tocas_dropdown = TocasFloatingUIDOM.autoUpdate(element, target, () => {
@@ -197,10 +197,10 @@ class Dropdown {
                 Object.assign(target.style, {
                     left: `${x}px`,
                     top: `${y}px`,
-                });
-            });
-        });
-    };
+                })
+            })
+        })
+    }
 }
 
 window.tocas_modules = [...window.tocas_modules, new Dropdown()]
--- src/scripts/tocas.tooltip.js
+++ src/scripts/tocas.tooltip.js
@@ -3,163 +3,195 @@ class Tooltip {
     attributeMutation = mutation => {
         // 如果追加的屬性包含 Tooltip 模組相關字樣，就監聽其互動事件。
         if (this.isTooltip(mutation.target)) {
-            this.bindEventListener(mutation.target);
+            this.bindEventListener(mutation.target)
         }
-    };
+    }
 
     // addedNodeMutation
     addedNodeMutation = added_node => {
         // 如果追加的 DOM 節點是一個 Tooltip 模組就監聽其互動事件。
         if (this.isTooltip(added_node)) {
-            this.bindEventListener(added_node);
+            this.bindEventListener(added_node)
         }
-    };
+    }
 
     // isTooltip
     isTooltip = element => {
-        return element.matches(`[${tocas.config.attributes.tooltip}]`);
-    };
+        return element.matches("[data-tooltip]")
+    }
 
     // bindEventListener
     bindEventListener = element => {
         // 重設這個元素的彈出提示計時器。
-        element.tocas_tooltip_timer = null;
+        element.tocas_tooltip_timer = null
 
         // 無論怎樣都先移除所有監聽事件，也不要取決於 [data-trigger]，
         // 因為新的跟舊的可能不一樣，到時候會有遺漏忘記的監聽器。
-        element.removeEventListener("mouseover", this.enterEventListener);
-        element.removeEventListener("mouseleave", this.leaveEventListener);
-        element.removeEventListener("focusin", this.enterEventListener);
-        element.removeEventListener("focusout", this.leaveEventListener);
+        element.removeEventListener("mouseover", this.enterEventListener)
+        element.removeEventListener("mouseleave", this.leaveEventListener)
+        element.removeEventListener("focusin", this.enterEventListener)
+        element.removeEventListener("focusout", this.leaveEventListener)
 
-        // 監聽移入跟移出的事件。
-        if (this.trigger(element) === "focus") {
-            element.addEventListener("focusin", this.enterEventListener);
-            element.addEventListener("focusout", this.leaveEventListener);
-        } else {
-            element.addEventListener("mouseover", this.enterEventListener);
-            element.addEventListener("mouseleave", this.leaveEventListener);
-        }
-    };
+        element.addEventListener("mouseover", this.enterEventListener)
+        element.addEventListener("mouseleave", this.leaveEventListener)
+        element.addEventListener("focusin", this.enterEventListener)
+        element.addEventListener("focusout", this.leaveEventListener)
+    }
 
     // delay
     delay = element => {
         // 從元素的屬性裡取得延遲的定義，如果是 0 就回傳 0。
         // 不直接丟給 parseInt 是因為可能會被當 false 值而回傳預設的 200ms。
-        var delay = element.getAttribute(tocas.config.attributes.tooltip_delay);
+        var delay = element.dataset.delay
         if (delay === "0") {
-            return 0;
+            return 0
         }
-        return parseInt(delay, 10) || 200;
-    };
+        return parseInt(delay, 10) || 200
+    }
 
     // position
     position = element => {
-        return element.getAttribute(tocas.config.attributes.tooltip_position) || "bottom";
-    };
+        return element.dataset.position || "bottom"
+    }
 
-    // trigger
-    trigger = element => {
-        return element.getAttribute(tocas.config.attributes.tooltip_trigger) || "hover";
-    };
+    // triggers
+    triggers = element => {
+        return element.dataset.trigger?.split(' ').filter(i => i) || ["hover"]
+    }
 
-    // enterEventListener
+    // hasTrigger
+    hasTrigger = (element, trigger) => {
+        return this.triggers(element).includes(trigger)
+    }
+
+    //
     enterEventListener = event => {
-        var element = event.target.closest(`[${tocas.config.attributes.tooltip}]`);
+        var type    = event.type
+        var element = event.target.closest("[data-tooltip]")
 
         // 如果目前的裝置是觸控裝置就忽略工具提示的觸發行為。
-        if (window.matchMedia("(pointer: coarse)").matches && this.trigger(element) === "hover") {
-            return;
+        if (type === "mouseover" && window.matchMedia("(pointer: coarse)").matches) {
+            return
+        }
+
+        // 如果滑鼠移入但是又沒有 Hover 觸發條件，就忽略滑鼠移入事件。
+        // 如果是 Focus 也是一樣的道理。
+        if ((type === "mouseover" && !this.hasTrigger(element, "hover")) ||
+            (type === "focusin"   && !this.hasTrigger(element, "focus"))) {
+            return
         }
 
         // 如果上一個工具提示的觸發計時器還存在或浮動元素還在的話，就忽略本次觸發行為，
         // 避免二次觸發而造成不可預期的錯誤。
         if (element.tocas_tooltip_timer !== null || element.tocas_tooltip !== undefined) {
-            return;
+            return
         }
 
         // 初始化一個會顯示工具提示的計時器，這樣滑鼠移入的數秒後就會顯示。
         element.tocas_tooltip_timer = setTimeout(() => {
-            this.showTooltip(element);
-        }, this.delay(element) + 1);
-    };
+            this.showTooltip(element)
+        }, this.delay(element) + 1)
+    }
 
-    // leaveEventListener
+    //
     leaveEventListener = event => {
-        var element = event.target.closest(`[${tocas.config.attributes.tooltip}]`);
-        var is_hover_trigger = this.trigger(element) === "hover";
+        var type    = event.type
+        var element = event.target.closest("[data-tooltip]")
 
-        // 如果離開的元素不是主元素就忽略，
-        // 如：使用者可能是離開了裡面的圖示元素，但滑鼠其實還在主元素裡。
-        if (event.target !== element && is_hover_trigger) {
-            return;
+        // 如果滑鼠移開的元素不是主元素就忽略，
+        // 因為移開事件會向上冒泡，所以可能是滑鼠移開了裡面的圖示元素，但滑鼠其實還在主元素裡。
+        if (type === "mouseleave" && event.target !== element) {
+            return
+        }
+
+        // 如果滑鼠移開這個元素，但這個元素有 Focus 觸發條件，且又還是在聚焦狀態，就忽略滑鼠移出事件
+        // 因為使用者可能是 Hover In 又 Hover Out，但是 Focus 更重要。
+        var has_focus_trigger = this.hasTrigger(element, "focus")
+        var focused_element   = document.activeElement.closest("[data-tooltip]")
+
+        if (type === "mouseleave" && has_focus_trigger && focused_element === element) {
+            return
         }
 
         // 如果浮動元素存在的話，就呼叫浮動元素的解除函式，然後歸零這個變數。
         if (element.tocas_tooltip !== undefined) {
-            element.tocas_tooltip();
-            element.tocas_tooltip = undefined;
+            element.tocas_tooltip()
+            element.tocas_tooltip = undefined
         }
 
         // 如果原先的計時器存在的話，就先重設，避免重複觸發。
         if (element.tocas_tooltip_timer !== null) {
-            clearTimeout(element.tocas_tooltip_timer);
-            element.tocas_tooltip_timer = null;
+            clearTimeout(element.tocas_tooltip_timer)
+            element.tocas_tooltip_timer = null
         }
 
-        // 依據觸發方式，移除對應的工具提示，
-        // 因為使用者可能在透過 Focus 觸發了某個工具提示，但是滑鼠又移入了另一個工具提示，
-        // 結果滑鼠移出時，連 Focus 的工具提示都被移除了。
-        document.querySelectorAll(is_hover_trigger ? `.ts-tooltip:not([data-trigger="focus"])` : `.ts-tooltip[data-trigger="focus"]`).forEach(tooltip => {
-            tooltip.remove();
-        });
-    };
+        // 取得這個工具提示的 ID。
+        var tooltip_id = element.getAttribute("aria-describedby")
+
+        // 從頁面上移除這個工具提示。
+        document.getElementById(tooltip_id)?.remove()
+
+        // 同時移除觸發元素聲明對應工具提示 ID 的輔助屬性。
+        element.removeAttribute("aria-describedby")
+    }
 
     // createTooltip
     createTooltip = (element, arrow) => {
-        var tooltip = document.createElement("div");
+        var tooltip = document.createElement("div")
 
         // 如果 [data-html] 是 "true" 的話就允許使用者在工具提示裡使用 HTML。
-        if (element.getAttribute(tocas.config.attributes.tooltip_html) === 'true') {
-            tooltip.innerHTML = element.getAttribute(tocas.config.attributes.tooltip);
+        if (element.dataset.html === 'true') {
+            tooltip.innerHTML = element.dataset.tooltip
         } else {
-            tooltip.innerText = element.getAttribute(tocas.config.attributes.tooltip);
+            tooltip.innerText = element.dataset.tooltip
         }
 
         // 標記這個工具提示被觸發的方式。
-        tooltip.setAttribute("data-trigger", this.trigger(element));
-
-        tooltip.classList.add("ts-tooltip", tocas.config.classes.tooltip_visible);
-        tooltip.append(arrow);
-        return tooltip;
-    };
+        tooltip.id = getID()
+        tooltip.classList.add("ts-tooltip", "is-visible")
+        tooltip.setAttribute("popover", "manual")
+        tooltip.append(arrow)
+        return tooltip
+    }
 
     // createArrow
     createArrow = () => {
-        var arrow = document.createElement("div");
-        arrow.classList.add("arrow");
-        return arrow;
-    };
+        var arrow = document.createElement("div")
+        arrow.classList.add("arrow")
+        return arrow
+    }
 
     // showTooltip
     showTooltip = element => {
         // 取得這個工具提示的位置設定。
-        var position = this.position(element);
+        var position = this.position(element)
 
         // 初始化工具提示的箭頭 DOM 元素。
-        var arrow = this.createArrow();
+        var arrow = this.createArrow()
 
         // 使用剛才建立的箭頭元素來初始化工具提示本身的 DOM 元素。
-        var tooltip = this.createTooltip(element, arrow);
+        var tooltip = this.createTooltip(element, arrow)
 
         // 將工具提示插入到網頁中。
-        document.body.append(tooltip);
+        document.body.append(tooltip)
+
+        // 使用 Popover API 才能在 Modal 或 Dialog, Popup 顯示，
+        // 不然會被蓋在 Top-Layer 下面。
+        tooltip.showPopover()
+
+        // 將工具提示插入到 element 的旁邊，這樣就不會被其他元素擋住。
+        // 例如：有些 element 在 Top-Layer。
+        // NOTE: 可能要注意這會不會害使用者的一些 :last-child 選擇器被破壞。
+        //element.parentNode.insertBefore(tooltip, element.nextSibling);
+
+        // 幫目前元素加上 aria-describedby 屬性，這樣螢幕閱讀器就會知道這個元素有工具提示。
+        element.setAttribute("aria-describedby", tooltip.id)
 
         // 使用 FloatingUI 來初始化工具提示的浮動元素。
         element.tocas_tooltip = TocasFloatingUIDOM.autoUpdate(element, tooltip, () => {
             TocasFloatingUIDOM.computePosition(element, tooltip, {
-                strategy: "fixed",
-                placement: position,
+                strategy  : "fixed",
+                placement : position,
                 middleware: [
                     // 下面過窄時會擠兌到上面。
                     TocasFloatingUIDOM.flip({
@@ -187,23 +219,23 @@ class Tooltip {
                 // 賦予工具提示絕對座標。
                 Object.assign(tooltip.style, {
                     left: `${x}px`,
-                    top: `${y}px`,
-                });
+                    top : `${y}px`,
+                })
 
                 // 設置箭頭的水平座標，因為箭頭只會出現在上面或下面，所以不需要 y 座標。
                 if (middlewareData.arrow) {
-                    const { x } = middlewareData.arrow;
-                    arrow.style.setProperty("--ts-tooltip-x", x != null ? `${x}px` : "0");
+                    const { x } = middlewareData.arrow
+                    arrow.style.setProperty("--ts-tooltip-x", x != null ? `${x}px` : "0")
                 }
 
                 // 先移除先前的所有位置設定，再套用新的位置設定。
                 if (placement) {
-                    tooltip.classList.remove("is-top", "is-top-start", "is-top-end", "is-bottom", "is-bottom-start", "is-bottom-end");
-                    tooltip.classList.add(`is-${placement}`);
+                    tooltip.classList.remove("is-top", "is-top-start", "is-top-end", "is-bottom", "is-bottom-start", "is-bottom-end")
+                    tooltip.classList.add(`is-${placement}`)
                 }
-            });
-        });
-    };
+            })
+        })
+    }
 }
 
 window.tocas_modules = [...window.tocas_modules, new Tooltip()]
